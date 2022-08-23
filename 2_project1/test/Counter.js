57a51fc94fea01d7359b75309bb8ac6ecdaa9378
Vikram <vikramamin22@gmail.com>
2022-08-23 21:47:23 +0530

    individual test of counter and name

--- 2_project1/test/Counter.js
+++ 2_project1/test/Counter.js
@@ -2,7 +2,11 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 describe('Counter', () => {
-    it('store the count', async () => {
+
+
+
+
+    it('Sets Initial count', async () => {
          // Load contract
     const Counter = await ethers.getContractFactory('Counter')
     const counter = await Counter.deploy('My Counter', 1)
@@ -10,4 +14,13 @@ describe('Counter', () => {
         expect(count).to.equal(1)
         // fetch  the count and check count it what we whant export
     })
+
+        it("sets initial name ", async () => {
+          // Load contract
+          const Counter = await ethers.getContractFactory("Counter");
+          const counter = await Counter.deploy("My Counter", 1);
+          const name = await counter.name();
+          expect(name).to.equal('My Counter');
+
+        });
 })
