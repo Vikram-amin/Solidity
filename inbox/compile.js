27fec509e4c7a789e3e46981cfc2128da7a306bf
Vikram <vikramamin22@gmail.com>
2022-09-26 22:37:56 +0530

    note

--- inbox/compile.js
+++ inbox/compile.js
@@ -7,4 +7,6 @@ const solc = require("solc");
 const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
 const source = fs.readFileSync(inboxPath, "utf8");
 
-module.exports = solc.compile(source, 1).contracts[":Index"];
+// module.exports = solc.compile(source, 1).contracts[":Index"];
+
+console.log(inboxPath)
--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -3,33 +3,60 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');   // constrctor function
-
 const web3 = new Web3(ganache.provider())
+const {interface, bytecode} = require('../compile')
 
 
-class Car {
-    park() {
-        return 'stopped';
-    }
 
-    drive(){
-        return 'vroom';
-    }
-}
+// class Car {
+//     park() {
+//         return 'stopped';
+//     }
 
-let car;
+//     drive(){
+//         return 'vroom';
+//     }
+// }
 
-beforeEach(() => {
-   car = new Car();
-})
+// let car;
+
+// beforeEach(() => {
+//    car = new Car();
+// })
 
 
-describe('Car', () => {
-    it('can park', ()=>{
-        assert.equal(car.park(), 'stopped');
+// describe('Car', () => {
+//     it('can park', ()=>{
+//         assert.equal(car.park(), 'stopped');
+//     });
+
+//     it('can drive', () => {
+//         assert.equal(car.drive(), 'vroom')
+//     })
+// });
+
+
+// ------------------------------------------
+
+let accounts;
+let inbox;
+
+beforeEach( async() => {
+ // get al list of all account 
+ accounts =  await web3.eth.getAccounts();
+
+ // use one of those accounts to deploy the contract
+  inbox = new web3.eth.Contract(JSON.parse(interface))
+  .deploy({data : bytecode, arguments : ['Hi there']})
+  .send({ from : accounts[0], gas: '1000000'})
+});
+
+
+describe('Index', () => {
+    it('deploys a contract', () => {
+        // console.log(accounts)
+        // console.log(inbox)
+
+        assert.ok(inbox.options.address);
     });
-
-    it('can drive', () => {
-        assert.equal(car.drive(), 'vroom')
-    })
 });
